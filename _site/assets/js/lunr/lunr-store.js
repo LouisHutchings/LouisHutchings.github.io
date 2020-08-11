fb815dc6eac763c41caa62895a95e32a1d890460
louishutchings <louishutchings6@gmail.com>
2020-08-11 19:19:40 +0100

    started first blog post

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -4,4 +4,10 @@ var store = [{
         "tags": ["test1","test2"],
         "url": "http://localhost:4000/TestPost/",
         "teaser": null
+      },{
+        "title": "Why Contact Tracing Apps Don't Need Your Location",
+        "excerpt":"As new cases of Covid-19 begin to drop in many countries, governments are looking for ways to loosen lockdown restrictions and restart the economy. Contact tracing of some form will need to be implemented if governments want to stop outbreaks before they begin and prevent a second wave. One solution...","categories": [],
+        "tags": ["Coronavirus","Covid-19","Contact Tracing"],
+        "url": "http://localhost:4000/ContactTracingApps/",
+        "teaser": null
       }]
